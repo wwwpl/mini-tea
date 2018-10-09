d0cef9106771eb88bb5657c44ecbcca5b6be736a
wangfei52 <Plpl.1007!>
2018-10-09 21:07:23 +0800

    修改购物车

--- miniprogram/pages/car/index.js
+++ miniprogram/pages/car/index.js
@@ -8,63 +8,110 @@ Page({
     cartGoods: ['1'],
     cartTotal: {
       "goodsCount": 1,
-      "goodsCountAmount":5
-    }
+      "goodsCountAmount": 5,
+      "goodsAllCount": 1
+    },
+    checkedAll: true,
+    goodsItems: [{
+        id: 1,
+        count: 1,
+        amount: 5,
+        checked: true,
+        url: "../../images/1.png",
+        name: '珍珠奶茶'
+      },
+      {
+        id: 2,
+        count: 1,
+        amount: 5,
+        checked: false,
+        url: "../../images/1.png",
+        name: '珍珠奶茶'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
-  }
+  },
+  checkboxChange: function(e) {
+   
+    var isCheck = this.isCheckedAll();
+   
+    if (isCheck==false){
+      for (var x in this.goodsItems) {//x = index
+        if (this.goodsItems[x].checked==false){
+          this.goodsItems[x].checked=true
+          }
+      }
+    }else{
+      for (var x in this.goodsItems) {//x = index
+        if (this.goodsItems[x].checked == true) {
+          this.goodsItems[x].checked = false
+        }
+      }
+    }
+  },
+  isCheckedAll: function () {
+    //判断购物车商品已全选
+    return this.data.goodsItems.every(function (element, index, array) {
+      if (element.checked == true) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  },
 })
